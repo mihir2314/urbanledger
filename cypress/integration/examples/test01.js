f9491b45fe91f3e53a28b7ea9c19dfc7b5458ee5
mihir2314 <mihirjadhav2311@gmail.com>
2023-03-22 18:58:31 +0530

    change

--- cypress/integration/examples/test01.js
+++ cypress/integration/examples/test01.js
@@ -1,25 +1,80 @@
 /// <reference types = "Cypress"/>
 import homePage from '../pageObject/accessHomepage'
+import 'cypress-iframe'
 
 describe('describe',()=>{
     Cypress.on('uncaught:exception', (err, runnable) => {
         return false;
     })
-    beforeEach(function(){
-        cy.viewport(1366,768)
-        const link = new homePage()
-        link.visitLink()    
-        cy.fixture('example').then(function(data){
-            this.data=data  
-            cy.get('#emailId').type(this.data.logincreds.email)
-            cy.get('#password').type(this.data.logincreds.pass)
-            cy.get('#login-btn').click()
-        })
-    })
+//     beforeEach(function(){
+//         cy.viewport(1366,768)
+//         const link = new homePage()
+//         link.visitLink()
+   
+//         cy.fixture('example').then(function(data){
+//             this.data=data  
+//             cy.get('#emailId').type(this.data.logincreds.email)
+//             cy.wait(1000)
+//             cy.get('#pass').type(this.data.logincreds.pass)
+//             cy.wait(1000)
+//             cy.frameLoaded("iframe[title='reCAPTCHA']")
+//             cy.iframe().find("span[role='checkbox']").click()
+//             cy.pause()
+// //           cy.get('[style="width: 304px; height: 78px;"] > div > iframe')
+           
+//         //     .then($iframe =>{
+//         //     const $body = $iframe.contents().find('body');
+//         //     cy.wrap($body)
+//         //     .find("span[role='checkbox']")
+//         //     .should('be.visible')
+//         //     .click()
+//         //    }
+//         //    )
+           
+//         })
+//         // cy.iframe.find('[style="width: 304px; height: 78px;"] > div > iframe').click()
+
+//         cy.get('#login-btn').click()
+//     })
 
 
 
     it('Orders',function(){
+        cy.viewport(1366,768)
+        const link = new homePage()
+        link.visitLink()
+   
+        cy.fixture('example').then(function(data){
+            this.data=data  
+            cy.get('#emailId').type(this.data.logincreds.email)
+            cy.wait(1000)
+            cy.get('#pass').type(this.data.logincreds.pass)
+            cy.wait(1000)
+            cy.frameLoaded("iframe[title='reCAPTCHA']")
+            cy.log("befor iframe")
+                      cy.iframe().first().click()
+                      cy.log("after iframe")
+                      // .should('be.visible')
+            //     .click({ force: true })
+            // cy.pause()
+            
+//           cy.get('[style="width: 304px; height: 78px;"] > div > iframe')
+           
+        //     .then($iframe =>{
+        //     const $body = $iframe.contents().find('body');
+        //     cy.wrap($body)
+        //     .find("span[role='checkbox']")
+        //     .should('be.visible')
+        //     .click()
+        //    }
+        //    )
+           
+        })
+        // cy.iframe.find('[style="width: 304px; height: 78px;"] > div > iframe').click()
+
+        cy.get('#login-btn').click()
+
+
         cy.get('[href="/orders"] > .MuiButtonBase-root').click({force:true})
         cy.get('.merchant_title').should('have.text','Orders')
     })
